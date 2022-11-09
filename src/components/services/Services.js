4a1a089a295d0e07d69723f38dd377492f5352af
Pankaj Yadav <91059277+iampankaj0@users.noreply.github.com>
2022-11-09 19:03:07 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/components/services/Services.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./services.scss";
+
+const Services = () => {
+  return (
+    <div className="services section" id="services">
+      <h2 className="section__title">Services</h2>
+      <span className="section__subtitle">What I Offer</span>
+    </div>
+  );
+};
+
+export default Services;
---
+++ src/components/skills/Backend.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend Developer</h3>
+
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">PHP</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">NODE</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">PYTHON</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">MONGO DB</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">FIREBASE</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">NO SQL</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
